9119247b4179ee9ca62c6afab58c053e0191b944
chenxingqiang <joy6677@qq.com>
2025-11-22 18:37:53 +0800

    Fix case-sensitive import in keyword_spotting_pacman.py
    - Changed 'from YZLITE' to 'from yzlite'
    - Fixes ModuleNotFoundError on case-sensitive filesystems
    
    Bump version to 0.2.3

--- yzlite/__init__.py
+++ yzlite/__init__.py
@@ -2,7 +2,7 @@
 import os
 import sys
 
-__version__ = '0.2.2'
+__version__ = '0.2.3'
 
 YZLITE_DIR = os.path.dirname(os.path.abspath(__file__)).replace('\\', '/')
 YZLITE_ROOT_DIR = os.path.dirname(YZLITE_DIR).replace('\\', '/')
