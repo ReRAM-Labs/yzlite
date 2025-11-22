4b09a9424a475b74307a1e0f25a2c494bf9be029
chenxingqiang <joy6677@qq.com>
2025-11-22 20:31:15 +0800

    Bump to v0.2.7

--- yzlite/__init__.py
+++ yzlite/__init__.py
@@ -2,7 +2,7 @@
 import os
 import sys
 
-__version__ = '0.2.6'
+__version__ = '0.2.7'
 
 YZLITE_DIR = os.path.dirname(os.path.abspath(__file__)).replace('\\', '/')
 YZLITE_ROOT_DIR = os.path.dirname(YZLITE_DIR).replace('\\', '/')
